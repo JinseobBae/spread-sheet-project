71a19610c520a79a30050434760de5be77d11f9d
JinseobBae <bbb1125@naver.com>
2021-05-27 20:36:24 +0900

    #3 add custom query in SheetJpaRepository

--- spreadsheet-web/src/main/java/com/js/spreadsheet/sheet/domain/SheetJpaRepository.java
+++ spreadsheet-web/src/main/java/com/js/spreadsheet/sheet/domain/SheetJpaRepository.java
@@ -2,5 +2,9 @@ package com.js.spreadsheet.sheet.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SheetJpaRepository extends JpaRepository<Sheet, Long> {
+
+    Optional<Sheet> findBySheetName(String sheetName);
 }
