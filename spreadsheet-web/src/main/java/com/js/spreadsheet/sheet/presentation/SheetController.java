253948d115985f8a789c3d15ce146980b3a96c42
JinseobBae <bbb1125@naver.com>
2021-06-01 17:08:11 +0900

    #4 Add SheetController

--- spreadsheet-web/src/main/java/com/js/spreadsheet/sheet/presentation/SheetController.java
+++ spreadsheet-web/src/main/java/com/js/spreadsheet/sheet/presentation/SheetController.java
@@ -1,18 +1,28 @@
 package com.js.spreadsheet.sheet.presentation;
 
+import com.js.spreadsheet.sheet.application.dto.RowDto;
+import com.js.spreadsheet.sheet.application.service.SheetSearchService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sheet/")
 public class SheetController {
 
 
+    private final SheetSearchService sheetSearchService;
+
+    public SheetController(SheetSearchService sheetSearchService){
+        this.sheetSearchService = sheetSearchService;
+    }
+
 
     @GetMapping("data")
-    void findSheetData(){
-
+    List<RowDto> findSheetData(RowDto rowDto){
+        return sheetSearchService.findRowData(rowDto);
     }
 
 }
