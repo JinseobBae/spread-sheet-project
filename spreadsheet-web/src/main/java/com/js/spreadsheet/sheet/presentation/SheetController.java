c967ce2cf39e3cff4b900d4f33c06ba4a03476c9
JinseobBae <bbb1125@naver.com>
2021-06-21 18:01:24 +0900

    #8 add controller for data control  - move search method to SheetSearchController

--- spreadsheet-web/src/main/java/com/js/spreadsheet/sheet/presentation/SheetController.java
+++ spreadsheet-web/src/main/java/com/js/spreadsheet/sheet/presentation/SheetController.java
@@ -1,28 +1,43 @@
 package com.js.spreadsheet.sheet.presentation;
 
+import com.js.spreadsheet.sheet.application.dto.ResponseDto;
 import com.js.spreadsheet.sheet.application.dto.RowDto;
-import com.js.spreadsheet.sheet.application.service.SheetSearchService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import com.js.spreadsheet.sheet.application.service.SheetUpdateService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sheet/")
 public class SheetController {
 
+    private final SheetUpdateService sheetUpdateService;
 
-    private final SheetSearchService sheetSearchService;
-
-    public SheetController(SheetSearchService sheetSearchService){
-        this.sheetSearchService = sheetSearchService;
+    public SheetController(SheetUpdateService sheetUpdateService){
+        this.sheetUpdateService = sheetUpdateService;
     }
 
+    @PostMapping("sheet")
+    ResponseDto addSheet(RowDto rowDto){
+        return sheetUpdateService.addSheet(rowDto);
+    }
 
-    @GetMapping("data")
-    List<RowDto> findSheetData(RowDto rowDto){
-        return sheetSearchService.findRowData(rowDto);
+    @DeleteMapping("sheet")
+    ResponseDto deleteSheet(RowDto rowDto){
+        return sheetUpdateService.removeSheet(rowDto);
+    }
+
+    @PostMapping("row")
+    ResponseDto addRow(RowDto rowDto){
+        return sheetUpdateService.addRow(rowDto);
+    }
+
+    @PutMapping("row")
+    ResponseDto updateRow(RowDto rowDto){
+        return sheetUpdateService.updateRow(rowDto);
+    }
+
+    @DeleteMapping("row")
+    ResponseDto deleteRow(RowDto rowDto){
+        return sheetUpdateService.removeRow(rowDto);
     }
 
 }
