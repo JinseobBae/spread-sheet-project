fa7c249723b33a050a977595a191ae41ef747297
JinseobBae <bbb1125@naver.com>
2021-06-29 18:36:51 +0900

    #12 add component in router (home)

--- spreadsheet-web/src/front-end/src/router/router.js
+++ spreadsheet-web/src/front-end/src/router/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Sheet from '../components/Sheet'
+import Home from '../components/HelloWorld'
 
 
 Vue.use(Router)
@@ -9,6 +10,11 @@ export default new Router({
     routes: [
         {
             path : '/',
+            name : 'Home',
+            component : Home
+        },
+        {
+            path : '/sheet/:name',
             name : 'Sheet',
             component : Sheet
         }
