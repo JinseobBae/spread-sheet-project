a6c775f740171486af0bf086e04bbf0fae6b9e8e
JinseobBae <bbb1125@naver.com>
2021-07-13 19:03:24 +0900

    #18 override updateCell method to add param(idx)

--- spreadsheet-web/src/front-end/src/editor/ExcelEditor.js
+++ spreadsheet-web/src/front-end/src/editor/ExcelEditor.js
@@ -6,8 +6,7 @@ Vue.use(VueExcelEditor)
 const BaseEditor = Vue.options.components["vue-excel-editor"]
 const ExcelEditor = BaseEditor.extend({
     methods: {
-        newRecord(rec, selectAfterDone, noLastPage, isUndo) {
-            console.error("rec", rec);
+        newRecord(rec) {
             if (typeof rec === 'undefined') rec = {}
             this.fields.map(f => {
                 if (typeof rec[f.name] === 'undefined') {
@@ -24,17 +23,77 @@ const ExcelEditor = BaseEditor.extend({
             this.value.splice(rowPos, 0, rec)
             this.table.splice(rowPos, 0, rec)
 
-            if (selectAfterDone) this.selected[rowPos] = id
-            Object.keys(rec).forEach(name => {
-                const field = this.fields.find(f => f.name === name)
-                if (field) this.updateCell(rec, field, rec[name], isUndo)
-            })
-            // this.refresh()
-            if (!noLastPage) this.lazy(() => {
-                // this.lastPage()
-                // this.moveToSouthWest()
-            })
             return rec
+        },
+        async updateCell (row, field, newVal, isUndo) {
+            switch(row.constructor.name) {
+                case 'String': // $id
+                    row = this.value.find(r => r.$id === row) // id
+                    break
+                case 'Number':
+                    row = this.table[row] // tablePos
+                    break
+                case 'Object': // record object
+                    break
+                default:
+                    throw new Error('Invalid row argument type')
+            }
+            switch(field.constructor.name) {
+                case 'String': // field name
+                    field = this.fields.find(f => f.name === field)
+                    break
+                case 'Number': // field pos
+                    field = this.fields[field]
+                    break
+                case 'Object': // field object
+                    break
+                default:
+                    throw new Error('Invalid field argument type')
+            }
+            if (!field) throw new Error('Invalid field argument')
+            if (!row) return // No row is found
+
+            const oldVal = row[field.name]
+            const oldKeys = this.getKeys(row)
+
+            if (field.change) {
+                let result = await field.change(newVal, oldVal, row, field)
+                if (result === false) return
+            }
+
+            row[field.name] =  newVal
+            console.error(row,field)
+            setTimeout(() => {
+                const transaction = {
+                    $id: row.$id,
+                    keys: this.getKeys(row),
+                    oldKeys: oldKeys,
+                    name: field.name,
+                    field: field,
+                    oldVal: typeof oldVal !== 'undefined' ? oldVal : '',
+                    newVal: newVal,
+                    err: '',
+                    idx: row.rowSeq || parseInt(row.pos) + 1
+                }
+
+                if (field.validate !== null) transaction.err = field.validate(newVal, oldVal, row, field)
+                if (field.mandatory && newVal === '')
+                    transaction.err += (transaction.err ? '\n' : '') + field.mandatory
+                this.setFieldError(transaction.err, row, field)
+
+                if (this.validate !== null) {
+                    transaction.rowerr = this.validate(newVal, oldVal, row, field)
+                    this.setRowError(transaction.rowerr, row)
+                }
+
+                if (field.summary)
+                    this.calSummary(field.name)
+
+                this.lazy(transaction, (buf) => {
+                    this.$emit('update', buf)
+                    if (!isUndo) this.redo.push(buf)
+                }, 50)
+            })
         }
     }
 })
