aa04d56dd8c2dd9eda574c22fc66aa6a0334dc60
JinseobBae <bbb1125@naver.com>
2021-07-22 14:07:36 +0900

    #25 redis config setting

--- spreadsheet-web/src/main/java/com/js/spreadsheet/SpreadsheetApplication.java
+++ spreadsheet-web/src/main/java/com/js/spreadsheet/SpreadsheetApplication.java
@@ -2,8 +2,10 @@ package com.js.spreadsheet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpreadsheetApplication {
 
     public static void main(String[] args) {
