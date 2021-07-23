80e38bd8b34eff049cdc90fcc9ad57d0bfb49908
JinseobBae <bbb1125@naver.com>
2021-07-23 15:17:39 +0900

    bootstrap add

--- spreadsheet-web/src/front-end/src/main.js
+++ spreadsheet-web/src/front-end/src/main.js
@@ -6,12 +6,16 @@ import "vue-navigation-bar/dist/vue-navigation-bar.css";
 import ExcelEditor from './editor/ExcelEditor'
 import ContextMenu from "@/components/ContextMenu";
 import ContextMenuItem from "@/components/ContextMenuItem";
+import BootstrapVue from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.component("vue-navigation-bar", VueNavigationBar);
 Vue.component('vue-excel-editor', ExcelEditor)
 Vue.component('context-menu', ContextMenu)
 Vue.component('context-menu-item', ContextMenuItem)
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
