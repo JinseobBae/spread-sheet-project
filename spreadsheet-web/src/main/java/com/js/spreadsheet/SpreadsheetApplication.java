760a5aa2e814ac0339d3c5725f7687dfd7655f0c
JinseobBae <bbb1125@naver.com>
2021-08-11 19:06:28 +0900

    merge SpreadsheetApplication.java with ServletInitializer.java

--- spreadsheet-web/src/main/java/com/js/spreadsheet/SpreadsheetApplication.java
+++ spreadsheet-web/src/main/java/com/js/spreadsheet/SpreadsheetApplication.java
@@ -2,14 +2,24 @@ package com.js.spreadsheet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableCaching
-public class SpreadsheetApplication {
+@EnableWebMvc
+public class SpreadsheetApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpreadsheetApplication.class, args);
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpreadsheetApplication.class);
     }
 
 }
