8efe00318576b8250b94dff4a6621f72138552b0
JinseobBae <bbb1125@naver.com>
2022-03-02 16:43:44 +0900

    modal 기초 추가

---
+++ spreadsheet-web/src/front-end/src/components/CategoryAdd.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="content">
+    <h1>카테고리 추가</h1>
+    <div>
+      <h2>Content</h2>
+      Lorem ipsum dolor sit amet,
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CategoryAdd",
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ spreadsheet-web/src/front-end/src/components/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="modal">
+    <div class="overlay" @click="$emit('close-modal')"></div>
+    <div class="modal-card">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: "CategoryAddModal"
+
+}
+</script>
+
+<style scoped>
+/* Modal */
+.modal{
+  display: inline-grid;
+  align-content: center;
+}
+.overlay {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+.overlay {
+  opacity: 0.5;
+  background-color: black;
+}
+.modal-card {
+  position: relative;
+  min-width: 40%;
+  max-width: 50%;
+  margin: auto;
+  margin-top: 30px;
+  padding: 20px;
+  background-color: white;
+  min-height: 200px;
+  z-index: 10;
+  opacity: 1;
+}
+</style>
